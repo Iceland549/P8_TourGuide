b5215e9e460f06ed788ad76c571db5ec819da62b
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-04-07 11:04:54 +0200

    Increased provider loop count to 10 and added 10 new provider names

--- TripPricer/TripPricer.cs
+++ TripPricer/TripPricer.cs
@@ -11,13 +11,14 @@ public class TripPricer
 {
     public List<Provider> GetPrice(string apiKey, Guid attractionId, int adults, int children, int nightsStay, int rewardsPoints)
     {
+        Console.WriteLine($"GetPrice called with attractionId: {attractionId}, adults: {adults}, children: {children}, nights: {nightsStay}, rewards: {rewardsPoints}");
         List<Provider> providers = new List<Provider>();
         HashSet<string> providersUsed = new HashSet<string>();
 
         // Sleep to simulate some latency
         Thread.Sleep(ThreadLocalRandom.Current.Next(1, 50));
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 10; i++)
         {
             int multiple = ThreadLocalRandom.Current.Next(100, 700);
             double childrenDiscount = children / 3.0;
@@ -37,12 +38,13 @@ public class TripPricer
             providersUsed.Add(provider);
             providers.Add(new Provider(attractionId, provider, price));
         }
+        Console.WriteLine($"GetPrice generated {providers.Count} providers");
         return providers;
     }
 
     public string GetProviderName(string apiKey, int adults)
     {
-        int multiple = ThreadLocalRandom.Current.Next(1, 10);
+        int multiple = ThreadLocalRandom.Current.Next(1, 20);
 
         return multiple switch
         {
@@ -55,6 +57,16 @@ public class TripPricer
             7 => "Live Free",
             8 => "Dancing Waves Cruselines and Partners",
             9 => "AdventureCo",
+            10 => "Global Getaways",
+            11 => "Skyhigh Tours",
+            12 => "Ocean Breeze Travel",
+            13 => "Wanderlux Agency",
+            14 => "Epic Journey",
+            15 => "Amazing",
+            16 => "Why Not",
+            17 => "Bucket List",
+            18 => "One Life",
+            19 => "No Way Home",
             _ => "Cure-Your-Blues",
         };        
     }
